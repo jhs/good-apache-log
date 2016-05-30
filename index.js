4623f5c48eafcd63a5be13dd99273c1ca69514f4
Jason Smith (iMac) <jason.h.smith@gmail.com>
2016-05-30 15:24:33 +0700

    First crack at porting to the new Good streaming API

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ module.exports = GoodApacheLog
 
 
 var fs = require('fs')
+var util = require('util')
 var Path = require('path')
 var debug = require('debug')('good:apache:log')
 var Stream = require('stream')
@@ -25,42 +26,36 @@ var OptionsSchema = Joi.alternatives().try(
 )
 
 
-function GoodApacheLog (events, config) {
+util.inherits(GoodApacheLog, Stream.Transform)
+function GoodApacheLog (config) {
   if (!(this instanceof GoodApacheLog))
     return new GoodApacheLog(events, config)
 
-  debug('Initialize log', {events:events, config:config})
+  var self = this
+  Stream.Transform.call(self, {objectMode: true})
 
   config = config || false
+  debug('Init %j', config)
   Joi.assert(config, OptionsSchema, 'Invalid options');
 
   if (typeof config == 'string' || config instanceof Stream.Stream)
     config = { file: config }
 
-  this._settings = Hoek.applyToDefaultsWithShallow(DEFAULTS, config, ['file'])
-  debug('Settings: %j', this._settings)
+  self._settings = Hoek.applyToDefaultsWithShallow(DEFAULTS, config, ['file'])
+  debug('Settings: %j', self._settings)
 
-  // Only process "response" events.
-  if (!events.response)
-    throw new Error('Invalid events; must configure "response" events, e.g. "response":"*"')
+  self._formatter = LogFormat(null, this._settings)
+  self._logfile = self._buildWriteStream()
 
-  this._streams = {
-    squeeze: Squeeze({response:events.response}),
-    formatter: LogFormat(null, this._settings)
-  }
-}
-
-GoodApacheLog.prototype.init = function (stream, emitter, callback) {
-  var self = this
-  debug('Init')
+  self.pipe(self._formatter)
+  self._formatter.pipe(self._logfile)
 
   if (!self._settings.hup)
     debug('No listen to SIGHUP')
   else {
     debug('Listen to SIGHUP')
-    self._onSigHUP = onSigHUP
     process.on('SIGHUP', onSigHUP)
-    emitter.on('stop', function() {
+    self.on('end', function() {
       debug('Events stopped; remove onSigHUP listener')
       process.removeListener('SIGHUP', onSigHUP)
     })
@@ -70,20 +65,20 @@ GoodApacheLog.prototype.init = function (stream, emitter, callback) {
     debug('Received SIGHUP')
     self._reopen()
   }
+}
 
-  self._streams.write = self._buildWriteStream()
-  self._streams.read = stream
-  self._pipeline()
-
-  callback()
+GoodApacheLog.prototype._reopen = function() {
+  debug('Re-open log file')
+  this._teardown()
+  this._logfile = this._buildWriteStream()
+  this._formatter.pipe(this._logfile)
 }
 
 GoodApacheLog.prototype._buildWriteStream = function () {
   var self = this
-  debug('buildWriteStream')
 
   if (self._settings.file instanceof Stream.Stream)
-    return self._settings.file;
+    return self._settings.file
 
   var result = fs.createWriteStream(self._settings.file, {flags:'a', end:false, encoding:'utf8'})
   result.once('error', function (er) {
@@ -94,22 +89,17 @@ GoodApacheLog.prototype._buildWriteStream = function () {
   return result
 }
 
-GoodApacheLog.prototype._reopen = function() {
-  debug('Re-open log file')
-  this._teardown()
-  this._streams.write = this._buildWriteStream()
-  this._pipeline()
-}
-
-GoodApacheLog.prototype._pipeline = function() {
-  this._streams.read
-    .pipe(this._streams.squeeze)
-    .pipe(this._streams.formatter)
-    .pipe(this._streams.write)
-}
-
 GoodApacheLog.prototype._teardown = function() {
-  this._streams.formatter.unpipe(this._streams.write)
-  this._streams.squeeze.unpipe(this._streams.formatter)
-  this._streams.read.unpipe(this._streams.squeeze)
+  this._streams.formatter.unpipe(this._logfile)
+}
+
+GoodApacheLog.prototype._transform = function(data, enc, callback) {
+  // Only process "response" events.
+  if (data.event != 'response') {
+    debug('Bad event for good-apache-log: %j', data.event)
+    return next()
+  }
+
+  this.push(data)
+  callback()
 }
